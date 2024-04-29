244d51934e15f4f82fdaf4e4af6b8caeb225e619
Arnav <arnavdoshi47@gmail.com>
2024-04-28 23:10:52 -0700

    fixes dependency

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -6,7 +6,6 @@ module com.example.demo {
     requires org.controlsfx.controls;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
-    requires jimObjModelImporterJFX;
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;

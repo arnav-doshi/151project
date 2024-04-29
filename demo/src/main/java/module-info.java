a670d6a4293004e8753acfe0bcb90e39b4bb2153
Arnav <arnavdoshi47@gmail.com>
2024-04-28 22:25:46 -0700

    Sid commit

---
+++ demo/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires jimObjModelImporterJFX;
+
+    opens com.example.demo to javafx.fxml;
+    exports com.example.demo;
+}
